b12db08ceccfec928c9d9b8584438f78e4c6aa51
Juan Pablo Forno <fornojp@gmail.com>
2022-06-26 23:29:53 -0300

    hice cambios

---
+++ src/components/ActPropiedades.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="jumbotron container">
+    <vue-form :state="formState" @submit.prevent="enviarActualizacion()">
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="domicilio">Tipo de Propiedad</label>
+        <input
+          type="text"
+          id="domicilio"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.tipo_de_propiedad"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+
+      <!-- Campo nombre -->
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="domicilio">Domicilio</label>
+        <input
+          type="text"
+          id="domicilio"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.domicilio"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="barrio">Barrio</label>
+        <input
+          type="text"
+          id="barrio"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.barrio"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="barrio">Tipo de Operacion</label>
+        <input
+          type="text"
+          id="barrio"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.tipo"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="dormitorios">Dormitorios</label>
+        <input
+          type="number"
+          id="dormitorios"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.dormitorios"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="antiguedad">Antiguedad</label>
+        <input
+          type="number"
+          id="antiguedad"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.antiguedad"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="condición">Condición</label>
+        <input
+          type="text"
+          id="condición"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.condición"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="precio">Precio</label>
+        <input
+          type="text"
+          id="precio"
+          name="tipo"
+          class="form-control"
+          v-model.trim="formData.precio"
+          autocomplete="off"
+          no-espacios
+        />
+
+        <!-- Mensajes de validacion -->
+        <field-messages name="nombre" show="$dirty"> </field-messages>
+      </validate>
+      <validate tag="div">
+        <!-- elemento de entrada -->
+        <label for="descripcion">Descripcion</label>
+        <textarea
+          name="descripcion"
+          id="descripcion"
+          cols="30"
+          rows="2"
+          v-model.trim="formData.descripcion"
+          class="form-control"
+        ></textarea>
+      </validate>
+
+      <br />
+
+      <div class="modal-footer">
+        <button
+          type="submit"
+          class="btn btn-success my-4"
+          :disabled="formState.$invalid"
+          @click="enviarActualizacion()"
+        >
+          Enviar
+        </button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+          Cerrar
+        </button>
+      </div>
+    </vue-form>
+  </div>
+</template>
+
+<script lang="">
+export default {
+  name: "src-components-act-propiedades",
+  props: [],
+  mounted() {},
+  data() {
+    return {
+      formData: this.$store.state.propiedadActualizar,
+      formState: {},
+    };
+  },
+  methods: {
+    enviarActualizacion() {
+      // console.log(this.formData);
+      this.$store.dispatch("actualizarPropiedad", this.formData);
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style scoped lang="css">
+.jumbotron {
+  background-color: #e2e2e2;
+  border-radius: 10px;
+  margin-top: 40px;
+}
+</style>
